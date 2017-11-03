541e393baf3d05fa8e3cc7014e605a8f1a784dcf
Nechay Olga <olganechay.bsu@gmail.com>
2017-11-03 20:45:42 +0300

    Project for count equal values was added

---
+++ Examination1/task_2/CountOfEqualValues/CounterOfEqualValues.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CountOfEqualValues
+{
+  class CounterOfEqualValues
+  {
+    private ArrayList collcetionOfValues;
+    public ArrayList CollcetionOfValues
+    { get; set; }
+
+    public void ReadFile(string currentLine)
+    {
+      CollcetionOfValues = new ArrayList();
+      using (StreamReader readLine = new StreamReader(currentLine))
+      {
+        while ((!readLine.EndOfStream))
+        {
+          CollcetionOfValues.Add(readLine.ReadLine());
+        }
+      }
+    }
+
+    public int CounterOfValues()
+    {
+      StringBuilder resultLine = new StringBuilder();
+      int counter = 0;
+      for(int i = 0; i < CollcetionOfValues.Count; i++)
+      {
+        for(int j = i + 1; j < CollcetionOfValues.Count; j++)
+        {
+          if (CollcetionOfValues[i].Equals(CollcetionOfValues[j]) || CollcetionOfValues[i] == CollcetionOfValues[j])
+          {
+            CollcetionOfValues.Remove(CollcetionOfValues[i]);
+            counter++;
+          }
+        }
+      }
+      foreach(object value in CollcetionOfValues)
+      {
+        resultLine.Append(value);
+      }
+      return counter;
+    }
+  }
+}
---
+++ Examination1/task_2/CountOfEqualValues/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountOfEqualValues
+{
+  class Program
+  {
+    const string StringValues = @"e:\StringValues.txt";
+    const string NumberValues = @"e:\NumberValues.txt";
+    static void Main(string[] args)
+    {
+      try
+      {
+        CounterOfEqualValues counterOfEqualValues = new CounterOfEqualValues();
+        counterOfEqualValues.ReadFile(StringValues);
+        Console.WriteLine(counterOfEqualValues.CounterOfValues());
+        counterOfEqualValues.ReadFile(NumberValues);
+        Console.WriteLine(counterOfEqualValues.CounterOfValues());
+        Console.ReadKey();
+      }
+      catch(Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
