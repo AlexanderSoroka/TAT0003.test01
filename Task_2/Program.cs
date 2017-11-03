162726619b5ef46561e648a02c438316a5890ae1
ButurlyaAL <stdlibh1421@gmail.com>
2017-11-03 19:49:02 +0200

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Task_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ReapetedElements reapetedElements = new ReapetedElements();
+      ArrayList list = new ArrayList() { 1, 2, 3 ,2,1,3,4,5,6,7,4,5};
+      Console.WriteLine(reapetedElements.GetReapetedElements(list));
+    
+    }
+  }
+}
---
+++ Task_2/ReapetedElements.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Task_2
+{
+  class ReapetedElements
+  {
+    public int GetReapetedElements(ArrayList list)
+    {
+      int count = 0;
+      ArrayList result = new ArrayList();
+      if (list.Count == 1)
+      {
+        return 1;
+      }
+      else if (list.Count == 0)
+      {
+        return 0;
+      }
+      list.Sort();
+      for (int i = 0; i < list.Count - 1; i++)
+      {
+        if (list[i].Equals(list[i + 1]) && !result.Contains(list[i]))
+        {
+          count++;
+        }
+      }
+      if (list[list.Count - 1].Equals(list[list.Count - 2]) && !result.Contains(list[list.Count - 1]))
+      {
+        count++;
+      }
+      return count;
+    }
+  }
+}
