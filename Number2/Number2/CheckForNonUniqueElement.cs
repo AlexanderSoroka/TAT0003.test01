3a0d778212465b6852035e5cf091aa5e166a4217
Volodko Julia <julia.volodko.2011@mail.ru>
2017-11-03 20:13:51 +0300

    Check: is every element in collection is unique or not unique.

---
+++ Number2/Number2/CheckForNonUniqueElement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Number2
+{
+  class CheckForNonUniqueElement
+  {
+   private List<int> NumbersForCheckin { get; set; }
+
+   public CheckForNonUniqueElement(List<int> numbersForChecking)
+    {
+      NumbersForCheckin = numbersForChecking;
+    }
+
+    //Check: is every element in collection is unique or not unique and add it(not unique) to the result collection.
+    public List<int> CheckTheNonUniqueElementOfArray(List<int> numbers)
+    {
+      List<int> resultArray = new List<int>();
+      for (int i = 0; i < numbers.Count; i++)
+      {
+        int unique = 0;
+        for (int j = 0; j < numbers.Count; j++)
+        {
+          if (numbers[i] == numbers[j])
+          {
+            unique++;
+          }
+        }
+        if (unique > 1)
+        {
+          if (!resultArray.Contains(numbers[i]))
+          {
+            resultArray.Add(numbers[i]);
+          }
+        }
+      }
+      return resultArray;
+    }
+
+    //For counting repetitive elements.
+    public int CountNonUniqueElement(List<int> arrayOfNonUniqueElement)
+    {
+      return arrayOfNonUniqueElement.Count;
+    }
+  }
+}
---
+++ Number2/Number2/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+namespace Number2
+{
+  //The entry point of the programm.
+  class EntryPoint
+  {
+    //For colling the methods for checking repetitive elements.
+    static void Main()
+    {
+      List<int> numbersForChecking = new List<int>() { 1, 1, 2, 3, 45, 2 };
+      CheckForNonUniqueElement checkNonUniqueElement = new CheckForNonUniqueElement(numbersForChecking);
+      List<int> arrayOfNonUniqueElement = checkNonUniqueElement.CheckTheNonUniqueElementOfArray(numbersForChecking);
+      Write(checkNonUniqueElement.CountNonUniqueElement(arrayOfNonUniqueElement));
+
+      ReadKey();
+    }
+  }
+}
