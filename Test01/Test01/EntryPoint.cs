8fc419c808a6c57e4e179f3c5f849ec4d972a852
HancharykNastassia <goncharik.anastasia@mail.ru>
2017-11-03 19:52:52 +0300

    Task 1

---
+++ Test01/Test01/EntryPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WayValidation
+{
+  class EntryPoint
+  {
+    static void Main (string[] args)
+    {
+      try
+      {
+        Validator validator = new Validator();
+        foreach (var line in args)
+        {
+          validator.ChecksIfFileWayContainsForbiddenSymbols(line);
+          if (validator.Valid)
+          {
+            StringBuilder output = new StringBuilder("The line\n" + line + "\nis valid");
+            Console.WriteLine(output);
+          }
+          else
+          {
+            StringBuilder output = new StringBuilder("The line\n" + line + "\nisn't valid");
+            Console.WriteLine(output);
+          }
+        }
+      }
+      finally
+      {
+        Console.WriteLine("Press any key for exit");
+        Console.ReadKey();
+      }
+    }
+  }
+}
---
+++ Test01/Test01/Validator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WayValidation
+{
+  class Validator
+  {
+    char[] foriddenSymbols = { '*', '|', '"', '<', '>', '?', '/' };
+    bool valid;
+    public bool Valid
+    {
+      get => valid;
+    }    
+    public void ChecksIfFileWayContainsForbiddenSymbols(string fileWay)
+    {
+      foreach(var symbol in foriddenSymbols)
+      {
+        if (fileWay.Contains(foriddenSymbols[symbol]))
+        {
+          valid = false;
+          break;
+        }
+        valid = true;
+      }
+    }
+  }
+}
