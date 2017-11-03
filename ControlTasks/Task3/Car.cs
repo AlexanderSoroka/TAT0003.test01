b54224c54d01bad808ef9b4f1423ed4e568d8db4
Lyudmila Ostrovskaya <lyuost97@gmail.com>
2017-11-03 21:08:56 +0300

    Add Task3. Realised only prise sort

---
+++ ControlTasks/Task3/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Car
+    {
+        private Params parameters;
+
+        Car(Params parameters)
+        {
+            this.parameters = parameters;
+        }
+
+        public Params GetParams()
+        {
+            return parameters;
+        }
+
+        public Car[] Sort(Car[] cars)
+        {
+            for (var i = 0; i < cars.Length - 1; i++)
+            {
+                if (cars[i].GetParams().Prise - cars[i+1].GetParams().Prise > Double.Epsilon)
+                {
+                    Car buff = cars[i];
+                    cars[i] = cars[i + 1];
+                    cars[i + 1] = buff;
+                }
+            }
+
+            return cars;
+        }
+    }
+}
---
+++ ControlTasks/Task3/Params.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    struct Params
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type
+        {
+            get
+            {
+                return Type;
+            }
+            set
+            {
+                if (Type != "sedan" || Type != "universal" || Type != "under")
+                {
+                    throw new Exception("You can't buy car with this type");
+                }
+                else
+                {
+                    this.Type = Type;
+                }
+            }
+        }
+        public double Prise { get; set; }
+        }
+    }
+}
