77d6111470c996ecd4b6f71a99b04445a1ddfe0a
HancharykNastassia <goncharik.anastasia@mail.ru>
2017-11-03 20:34:29 +0300

    task 2

---
+++ Test01/RepeatingObjects/EntryPoint.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatingObjects
+{
+  class EntryPoint
+  {
+    static void Main (string[] args)
+    {
+      const int MaxSizeOfList = 100;
+      List<string> listOfStrings = new List<string>();
+      bool stop = false;
+      do
+      {
+        string inputLine = Console.ReadLine();
+        if (inputLine.Equals(String.Empty))
+        {
+          stop = true;
+        }
+        else
+        {
+          listOfStrings.Add(inputLine);
+        }
+        if (listOfStrings.Capacity == MaxSizeOfList)
+        {
+          stop = true;
+        }
+      }
+      while (!stop);
+      List<string> counted = new List<string>();
+      foreach (var str in listOfStrings)
+      {
+        if (!counted.Contains(str))
+        {
+          Searcher searcher = new Searcher(str, listOfStrings);
+          searcher.CountReapitings();
+          int quantityOfReapitings = searcher.QuantityOfReapitingObjects;
+          StringBuilder output = new StringBuilder("Line " + str + " repeats for " + quantityOfReapitings 
+                                                    + " times");
+          Console.WriteLine(output);
+          counted.Add(str);
+        }
+      }
+      Console.WriteLine("Press any key for exit");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Test01/RepeatingObjects/Searcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatingObjects
+{
+  class Searcher
+  {
+    string lineForSearch;
+    List<string> ListToSearchIn;
+    int quantityOfReapitingObjects;
+    public int QuantityOfReapitingObjects
+    {
+      get => quantityOfReapitingObjects;
+    }
+    public Searcher(string line, List<string> list)
+    {
+      lineForSearch = line;
+      ListToSearchIn = list;
+      quantityOfReapitingObjects = 0;
+    }
+    public void CountReapitings ()
+    {
+      foreach (var line in ListToSearchIn)
+      {
+        if (line.Equals(lineForSearch))
+        {
+          quantityOfReapitingObjects += 1;
+        }
+      }
+    }
+  }
+}
