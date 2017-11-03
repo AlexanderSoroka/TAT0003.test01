f14ff47b15c7bc1fabb80359ccfa8cf47456636a
Lyudmila Ostrovskaya <lyuost97@gmail.com>
2017-11-03 19:59:39 +0300

    New project Task1

---
+++ ControlTasks/Task1/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            if(Directory.Exists(args[0]))
+            {
+                Console.WriteLine("It is valid path");
+            }
+            else
+            {
+                Console.WriteLine("It is non-valid path");
+            }
+        }
+    }
+}
