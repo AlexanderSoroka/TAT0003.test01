2b64b758753db30e8a9c8899775673e80ab2b96d
HelenPribysh <helenpribysh@gmail.com>
2017-11-03 20:57:55 +0300

    task1

---
+++ task1/task1/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class EntryPOint
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string pathFromConsole = Console.ReadLine();
+                ValidatoinClass validPath = new ValidatoinClass(pathFromConsole);
+                Console.WriteLine(validPath.isValidFilePath());
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ task1/task1/ValidatoinClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class ValidatoinClass
+    {
+        private string pathFromConsole;
+        const string SPECIALSIGNS = "*|\"<>/?%";
+        const string FIRSTSYMBOL = "CDEF";
+        public ValidatoinClass(string pathFromConsole)
+        {
+            this.pathFromConsole = pathFromConsole;
+        }
+        //this ValidatoinClass:isValidFilePat method checks if the path is valid
+        public bool isValidFilePath()
+        {
+            try
+            {
+                if (!FIRSTSYMBOL.Contains(getFirstSymbol(pathFromConsole)))
+                    return false;
+
+                for (int i = 0; i <= pathFromConsole.Length - 1; i++)
+                {
+                    if (SPECIALSIGNS.Contains(pathFromConsole[i]))
+                    {
+                        return false;
+                    }
+                }                
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return true;
+                
+        }
+        //this ValidatoinClass:getFirstSymbol method returns first symbol of entry string
+        private char getFirstSymbol(string path)
+        {
+            return path[0];
+        }
+
+    }
+}
