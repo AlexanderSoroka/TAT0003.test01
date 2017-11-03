c85fde50c3243e58b542ba5b3cb3c655958e17e3
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-03 20:23:04 +0300

    added Task2

---
+++ Tasks/Task2/CollectionAnalyzer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+  /// <summary>
+  /// Class contains method
+  /// that collects in dictionary elements from input collection
+  /// and help method
+  /// that remove from dictionary non duplicated elements
+  /// </summary>
+  public class CollectionAnalyzer
+  {
+    public Dictionary<T, int> DuplicatedItemsDictionary<T>(IEnumerable<T> collection)
+    {
+      Dictionary<T,int> resultDictionary = new Dictionary<T, int>();
+
+      foreach (var item in collection)
+      {
+        var count = 0;
+        resultDictionary.TryGetValue(item, out count);
+        resultDictionary[item] = count + 1;
+      }
+
+      return RemoveNonDuplicatedItemsFromDictionary(resultDictionary);
+    }
+
+    private Dictionary<T, int> RemoveNonDuplicatedItemsFromDictionary<T>(Dictionary<T, int> dictionary)
+    {
+      Dictionary<T, int> resultDictionary = dictionary;
+      foreach (var item in dictionary.Where(item => item.Value == 1).ToList())
+      {
+        resultDictionary.Remove(item.Key);
+      }
+      return resultDictionary;
+    }
+  }
+}
---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+  /// <summary>
+  /// Main method of this class
+  /// calls method of class CollectionAnalyser
+  /// that calculates the number of repetitive items in collections
+  /// </summary>
+  public class EntryPoint
+  {
+    private const string Item = "Item: ";
+    private const string Repeats = "Repeats: ";
+
+    /// <summary>
+    /// Method displays the result
+    /// of checking the input collection for duplicates
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+      try
+      {
+        List<int> testedCollectionInts = new List<int>();
+
+        Dictionary<int,int> duplicatedItemsDictionary = (new CollectionAnalyzer()).DuplicatedItemsDictionary(testedCollectionInts);
+
+        foreach (var item in duplicatedItemsDictionary)
+        {
+          Console.WriteLine($"{Item}{item.Key} {Repeats}{item.Value}\n");
+        }
+        Console.ReadKey();
+      }
+      catch (Exception exc)
+      {
+        Console.WriteLine(exc.Message);
+      }
+    }
+  }
+}
