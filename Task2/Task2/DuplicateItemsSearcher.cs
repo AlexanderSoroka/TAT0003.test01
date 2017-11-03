7888e0d5c3dc6e7e17d9dcada831767f89fcb76f
KlyuchevenkoEkaterina <ktrin1996@mail.ru>
2017-11-03 20:34:13 +0200

    Add Task2

---
+++ Task2/Task2/DuplicateItemsSearcher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+  //This class is used to find and consume duplicate items and number of repetitions on the console.
+  class DuplicateItemsSearcher
+  {
+    //This method is used to find duplicate items.
+    public List<int> SearcherDuplicateItems(List<int> listNumbers, List<int> listResult)
+    {
+      for (int i = 0; i < listNumbers.Count; i++)
+      {
+        int copy = 0;
+        for (int j = 1; j < listNumbers.Count; j++)
+        {
+          if (listNumbers[i] == listNumbers[j])
+          {
+            copy++;
+          }
+        }
+        if (copy >= 2)
+        {
+          listResult.Add(listNumbers[i]);
+        }
+      }
+      return listResult;
+    }
+
+    //This method is used to output duplicate items.
+    public void OutputDuplicateItems(List<int> listResult)
+    {
+      foreach (int item in listResult)
+      {
+        Console.Write(item);
+      }
+    }
+
+    //This method is used to output number of repetitions.
+    public void OutputNumOfRepetitions(List<int> listResult)
+    {
+      int number = 0;
+      for (int i = 0; i < listResult.Count; i++)
+      {
+        if (listResult.Contains(i))
+        {
+          number++;
+        }
+      }
+      Console.WriteLine("\nNumber of repetitions: " + number);      
+    }
+  }
+}
---
+++ Task2/Task2/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+  //The entry point of the program.
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      List<int> listNumbers = new List<int>() { 2, 2, 3, 45, 2, 3, 81 };
+      List<int> listResult = new List<int>();
+      DuplicateItemsSearcher duplicateItemsSearcher = new DuplicateItemsSearcher();
+      List<int> listDuplicateItems = duplicateItemsSearcher.SearcherDuplicateItems(listNumbers, listResult);
+      duplicateItemsSearcher.OutputDuplicateItems(listDuplicateItems);
+      duplicateItemsSearcher.OutputNumOfRepetitions(listDuplicateItems);
+      Console.ReadKey();
+    }
+  }
+}
