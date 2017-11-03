293a2cd32309e9b76b7ec9acecfe961063bf4348
Nalivkin Igor <igor.nalivkin-2010@yandex.by>
2017-11-03 16:46:00 +0300

    task 1

---
+++ task1/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      string path = args[0];
+      PathValidator validator = new PathValidator(args[0]);
+      Console.Write(validator.IsPathValid);
+      Console.ReadKey();
+    }
+  }
+}
---
+++ task1/PathValidator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+  class PathValidator
+  {
+    public bool IsDiskNameCorrect {get; set; }
+    public bool IsPathValid { get; set; }
+    string[] pathPieces;
+    public PathValidator(string path)
+    {
+      pathPieces = path.Split('\\');
+      IsDiskNameCorrect = DiskNameChecker(path);
+      IsPathValid = PathValidateChecker(pathPieces);
+    }
+
+    private bool DiskNameChecker(string path)
+    {
+      return (pathPieces[0][pathPieces[0].Length - 1] == ':');
+    }
+    private bool PathValidateChecker(string[] pathPieces)
+    {
+      string notValidSymbols = "\\/:*?\"<>|";
+      for (int i = 1; i < pathPieces.Length; i++)
+      {
+        for (int j = 0; j < pathPieces[i].Length; j++)
+        {
+          for (int k=0;k<notValidSymbols.Length;k++)
+          {
+             if(pathPieces[i][j]==notValidSymbols[k])
+               return false;
+          }
+        }
+      }
+      return true;
+    }
+  }
+}
