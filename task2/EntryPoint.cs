41eb340f39f309e1535b98f72318219942155678
Nalivkin Igor <igor.nalivkin-2010@yandex.by>
2017-11-03 17:26:35 +0300

    task 2

---
+++ task2/EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      List<int> collection = new List<int>{ 1, 2, 3, 1, 2, 3, 1, 2, 3 };
+      RepeatsChecker repeatsChecker = new RepeatsChecker(collection);
+      Console.Write(repeatsChecker.GetRepetitiveElementsCount());
+    }
+   
+  }
+}
---
+++ task2/RepeatsChecker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+  class RepeatsChecker
+  {
+    // This class is used for detect the repeats in
+    // List<int> collection.
+    public List<int> Collection { get; set; }
+    public List<int> RepetitiveElements { get; set; }
+    public RepeatsChecker(List<int> collection)
+    {
+      this.Collection = collection;
+      RepetitiveElements = new List<int>();
+      FindRepetitiveElements();
+    }
+    // This method detects the repeats and write them down
+    // into RepetetiveElements List.
+    private void FindRepetitiveElements()
+    {
+      for (int i = 0; i < Collection.Count; i++)
+      {
+        bool flag = false;
+        for (int j = i + 1; j < Collection.Count; j++)
+        {
+          if (Collection[i] == Collection[j])
+          {
+            flag = true;
+            break;
+          }
+        }
+        if (flag && !RepetitiveElements.Contains(Collection[i]))
+        {
+          RepetitiveElements.Add(Collection[i]);
+        }
+      }
+    }
+    // This method is used for getting the count of 
+    // repetitive elements.
+    public int GetRepetitiveElementsCount()
+    {
+      return RepetitiveElements.Count;
+    }
+  }
+}
