5e635d2e0e566f5b60dc15d9bc3c464e9c04e232
Vadim Belkin <vortorent@mail.ru>
2017-11-03 20:06:16 +0300

    The first task

---
+++ PathValidation/CheckingOfPath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PathValidation
+{
+  /// <summary>
+  /// This class realizes to validation of the path. 
+  /// </summary>
+  class CheckingOfPath
+  {
+    /// <summary>
+    /// This method checks whether the path is correct.
+    /// </summary>
+    /// <param name="path">Path, which need check.</param>
+    /// <returns>Returns true, if path contains invalid symbols and false if doesn't contain.</returns>
+    public bool СheckOfPath(string path)
+    {
+      for (int i = 0; i < Path.GetInvalidPathChars().Length; i++)
+      {
+        if (path.Contains(Path.GetInvalidPathChars()[i]))
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+}
---
+++ PathValidation/EntryPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PathValidation
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+
+      Console.WriteLine("Enter path to file:");
+      args = new string[1];
+      args[0] = Console.ReadLine();
+
+      CheckingOfPath inspectorOfPath = new CheckingOfPath();
+
+      if (inspectorOfPath.СheckOfPath(args[0]))
+      {
+        Console.WriteLine("This path is not exist.");
+      }
+      else
+      {
+        Console.WriteLine("This path is exist.");
+      }
+    }
+  }
+}
