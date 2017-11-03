d6d33e5f2465267f2dc8b5efe2576e0b76e4ef2b
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-03 19:27:14 +0300

    added Task1

---
+++ Tasks/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task1
+{
+  /// <summary>
+  /// This class contain method
+  /// that checks the path to the file
+  /// passed as a command-line argument
+  /// </summary>
+  public class EntryPoint
+  {
+    private const string InvalidFileName = "Invalid file name";
+
+    /// <summary>
+    /// Method contain two if blocks
+    /// First checks that command line is not empty
+    /// Second checks that the path is valid
+    /// </summary>
+    /// <param name="args">The path to file</param>
+    static void Main(string[] args)
+    {
+      try
+      {
+        if (args.Length == 0)
+        {
+          throw new ArgumentNullException();
+        }
+        if (args[0].IndexOfAny(System.IO.Path.GetInvalidFileNameChars()) != -1)
+        {
+          throw new Exception(InvalidFileName);
+        }
+      }
+      catch (Exception exc)
+      {
+        Console.WriteLine(exc.Message);
+      }
+    }
+  }
+}
